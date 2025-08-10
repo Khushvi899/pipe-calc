4549671ff45586ca4c5a33f88423011a8e0c483a
Khushvi899 <khushbusathvara@gmail.com>
2025-08-10 22:38:56 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'pe-pipe-calculator-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+});
